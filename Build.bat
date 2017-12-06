e8600d48b5f7d006f61821f7db465658329de54d
Michael Kriese <michael.kriese@visualon.de>
2017-12-06 10:57:55 +0100

    Use dotnet to build.

--- Build.bat
+++ Build.bat
@@ -1,11 +1,1 @@
-@rem nuget pack Unity.Wcf\Unity.Wcf.csproj -properties Configuration=Release -symbols -build -OutputDirectory bin %*
-
-@echo off
-
-for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
-  set InstallDir=%%i
-)
-
-if exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
-  "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" Unity.Wcf.sln /nologo /p:Configuration=Release /m /v:m %*
-)
+dotnet pack %*
